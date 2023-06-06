9a854cc6dd83f098a1b1bd91325f1960600aa6af
Adam <adam.shelley18@gmail.com>
2023-06-06 10:28:13 +0800

    Media query on desktop fix

--- src/HTML.jsx
+++ src/HTML.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Lenis as ReactLenis } from "@studio-freight/react-lenis";
